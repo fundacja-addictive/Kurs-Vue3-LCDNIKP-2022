43795aaea61323aa41993e3fe6e89a6207a69e9d
Karol Sobański <chomiciak@gmail.com>
2022-12-08 19:19:24 +0100

    Adding ship picking that works

--- src/components/ShipPicker.vue
+++ src/components/ShipPicker.vue
@@ -17,7 +17,7 @@ export default {
     },
     data: function () {
         return {
-            markedFields: [],
+            markedFields: null,
             ships: [
                 {
                     id: 1,
@@ -74,13 +74,111 @@ export default {
     },
     mounted: function () {
         this.ships[0].state = 'current';
+
+        this.markedFields = [];
+
     },
     methods: {
         elementClicked: function (element) {
-            console.log("Clicked AGAIN", element);
-            if (element.marked)
+            if (element.marked == true)
                 this.markedFields.push(element);
+            else {
+                var e = this.markedFields.findIndex(function (elem) {
+                    return elem.x == element.x && elem.y == element.y;
+                });
+                this.markedFields.splice(e, 1);
+            }
+        },
+        findCurrentShip: function () {
+            return this.ships.find(s => {
+                return s.state == 'current';
+            });
+        },
+        setNextCurrent: function () {
+            var free = this.ships.find(s => s.state == 'free');
+
+            if (!free)
+                return false;
+
+            free.state = 'current';
+            
+            return true;
+        },
+        validateShip: function (shipArray) {
+            var curr = this.findCurrentShip();
+
+            if (!curr)
+                return alert("Gratulacje!");
+
+            if (curr.length == shipArray.length) {
+                curr.state = 'picked';
+                curr.nodes = shipArray;
+                this.markedFields = [];
+
+                this.setNextCurrent();
+            }
         },  
+    },
+    watch: {
+        markedFields: {
+            deep: true,
+            handler: function (fields) {
+                console.log(fields);
+
+                var nodes = [];
+
+                // U, D, L, R
+                var findField = (fieldsToSearch, currentField, direction) => {
+                    var checkX = currentField.x.charCodeAt(0) 
+                        if (direction == 'L') 
+                            checkX = checkX - 1;
+                        if (direction == 'R')
+                            checkX = checkX + 1;
+                        checkX = String.fromCharCode(checkX);
+                    var checkY = currentField.y + 0;
+                        if(direction == 'U')
+                            checkY += 1;
+                        if(direction == 'D')
+                            checkY -= 1;
+
+                    var found = fieldsToSearch.find((e) => {
+                        return e.x == checkX && e.y == checkY;
+                    })
+
+                    return found;
+                };
+
+                var checkOther = (field /* object */, directions /* array */) => {
+                    var found = nodes.find(e => e.x == field.x && e.y == field.y);
+                    if (found)
+                        return;
+
+                    nodes.push(field);
+
+                    directions.forEach(d => {
+                        var result = findField(fields, field, d);
+
+                        if (result) {
+                            if (d == 'U' || d == 'D')
+                                checkOther(result, ["U", "D"]);
+                            if (d == 'L' || d == 'R')
+                                checkOther(result, ["L", "R"]);
+                            return;
+                        }
+                    });
+                };
+
+                if (fields.length > 0)
+                    checkOther(fields[0], [
+                        "U", 
+                        "D", 
+                        "L", 
+                        "R"
+                    ]);
+
+                this.validateShip(nodes);
+            },
+        }
     }
 }
 </script>
